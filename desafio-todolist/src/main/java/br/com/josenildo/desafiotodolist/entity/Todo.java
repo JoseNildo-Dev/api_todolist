3e77eaa22140833b36895eb434da62e0419c36c1
José Nildo de Macedo Pereira <Jos Nildo de Macedo Pereira>
2023-11-27 09:35:15 -0300

    Add: Adicionar lombok e configurar db para rodar via docker

--- desafio-todolist/src/main/java/br/com/josenildo/desafiotodolist/entity/Todo.java
+++ desafio-todolist/src/main/java/br/com/josenildo/desafiotodolist/entity/Todo.java
@@ -2,9 +2,15 @@ package br.com.josenildo.desafiotodolist.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "todos")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Todo {
 
     @Id
@@ -23,46 +29,6 @@ public class Todo {
         this.nome = nome;
         this.descricao = descricao;
         this.realizado = realizado;
-        this.prioridade = prioridade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public boolean isRealizado() {
-        return realizado;
-    }
-
-    public void setRealizado(boolean realizado) {
-        this.realizado = realizado;
-    }
-
-    public int getPrioridade() {
-        return prioridade;
-    }
-
-    public void setPrioridade(int prioridade) {
         this.prioridade = prioridade;
     }
 }
